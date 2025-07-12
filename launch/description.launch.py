68b78429248006ad24f59f38355d58a857c0c7e0
Suraj Dayma <38291736+suraj1102@users.noreply.github.com>
2025-07-12 21:40:47 +0530

    Update description from parameters.yaml

--- launch/description.launch.py
+++ launch/description.launch.py
@@ -1,5 +1,6 @@
 import os
 import xacro
+import yaml
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
@@ -7,14 +8,24 @@ from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
 from ament_index_python.packages import get_package_share_directory
 
+def load_robot_description(robot_description_path, vehicle_params_path):
+    with open(vehicle_params_path, 'r') as file:
+        vehicle_params = yaml.safe_load(file)['/**']['ros__parameters']
+
+    robot_description = xacro.process_file(
+        robot_description_path,
+        mappings={key: str(value) for key, value in vehicle_params.items()}
+    )
+
+    return robot_description.toxml()
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('gazebo_ackermann_steering_vehicle')
+    package_name = "gazebo_ackermann_steering_vehicle"
+    package_path = get_package_share_directory(package_name)
+    xacro_file = os.path.join(package_path, 'model', 'vehicle.xacro')
+    vehicle_params_path = os.path.join(package_path, 'config', 'parameters.yaml')
 
-    xacro_file = os.path.join(share_dir, 'model', 'vehicle.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_urdf = robot_description_config.toxml()
-
+    robot_urdf = load_robot_description(xacro_file, vehicle_params_path)
 
     # Launch Arguments
     state_publisher_rename_arg = DeclareLaunchArgument(
